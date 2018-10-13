a637f851f3c3249919c01d43108dbc6a10e3329a
sachi <sachi@DESKTOP-EBA4QC0>
2018-10-13 22:47:04 +0530

    Added decorator pattern to implement counter for duck quacks

---
+++ src/compoundpatternsexample1/QuackCounter.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package compoundpatternsexample1;
+
+/**
+ *
+ * @author sachi
+ */
+public class QuackCounter implements Quackable{
+
+    Quackable duck;
+    static int noOfQuacks;
+
+    public QuackCounter(Quackable duck) {
+        this.duck=duck;
+    }
+    
+    
+    @Override
+    public void quack() {
+        duck.quack();
+        noOfQuacks++;
+    }
+    
+    public static int getQuackCount(){
+        return noOfQuacks;
+    }
+    
+}
